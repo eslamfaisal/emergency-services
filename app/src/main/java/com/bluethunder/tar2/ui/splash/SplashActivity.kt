7ca76576e22d087dd0fd4ffcfd526cf2627fc32f
Eslam Faisal <eslam.faisal.ef@gmail.com>
2022-07-10 15:43:33 +0200

    open main activity with animation

--- app/src/main/java/com/bluethunder/tar2/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/bluethunder/tar2/ui/splash/SplashActivity.kt
@@ -25,10 +25,8 @@ class SplashActivity : AppCompatActivity() {
     private fun openHomeActivity() {
         val intent = Intent(this, MainActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-        startActivity(
-            intent,
-            ActivityOptions.makeSceneTransitionAnimation(this).toBundle()
-        )
+        startActivity(intent)
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         finish()
     }
 }
