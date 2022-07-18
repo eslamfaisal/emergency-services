149202aca6deb8b73a3cf8201b98d71ed5f64c0a
Eslam Faisal <eslam.faisal.ef@gmail.com>
2022-07-19 00:44:22 +0200

    design case details

--- app/src/main/java/com/bluethunder/tar2/ui/edit_case/fragments/CaseDetailsFragment.kt
+++ app/src/main/java/com/bluethunder/tar2/ui/edit_case/fragments/CaseDetailsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.bluethunder.tar2.R
 import com.bluethunder.tar2.databinding.FragmentCaseDetailsBinding
@@ -30,6 +31,13 @@ class CaseDetailsFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initViews()
+    }
+
+    private fun initViews() {
+        binding.caseCategoryInput.setOnClickListener {
+            Toast.makeText(context, "Clicked", Toast.LENGTH_SHORT).show()
+        }
     }
 
 
