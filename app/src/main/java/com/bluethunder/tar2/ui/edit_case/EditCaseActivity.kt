ce4a96fd878b2dbec7ae1e220bc6bbce0f86743a
Eslam Faisal <eslam.faisal@algodriven.xyz>
2022-07-19 11:32:00 +0200

    get location

--- app/src/main/java/com/bluethunder/tar2/ui/edit_case/EditCaseActivity.kt
+++ app/src/main/java/com/bluethunder/tar2/ui/edit_case/EditCaseActivity.kt
@@ -1,22 +1,18 @@
 package com.bluethunder.tar2.ui.edit_case
 
 import android.Manifest
+import android.app.Activity
 import android.content.Intent
-import android.content.IntentSender.SendIntentException
 import android.content.pm.PackageManager
 import android.os.Bundle
-import android.os.Looper
 import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.bluethunder.tar2.databinding.ActivityEditCaseBinding
+import com.bluethunder.tar2.model.Status
 import com.bluethunder.tar2.ui.edit_case.viewmodel.EditCaseViewModel
 import com.bluethunder.tar2.ui.extentions.getViewModelFactory
-import com.huawei.hmf.tasks.OnSuccessListener
-import com.huawei.hms.common.ApiException
-import com.huawei.hms.common.ResolvableApiException
-import com.huawei.hms.location.*
 
 
 class EditCaseActivity : AppCompatActivity() {
@@ -48,10 +44,28 @@ class EditCaseActivity : AppCompatActivity() {
     }
 
     private fun initObservers() {
-        viewModel.onSelectedTabIndex.observe(this) {
+        viewModel.checkDeviceLocation(this)
+        viewModel.deviceLocationCheck.observe(this) { resource ->
+            when (resource.status) {
+                Status.SUCCESS -> {
+                    requestLocationPermission()
+                }
+                Status.ERROR -> {
+                    showRequestDeviceLocationErrorDialog()
+                }
+                Status.LOADING -> {
+                    Log.e("EditCaseActivity", "Loading")
+                }
+                else -> {
+                    Log.e("EditCaseActivity", "Unknown error")
+                }
+            }
 
         }
-        checkDeviceLocation()
+    }
+
+    private fun showRequestDeviceLocationErrorDialog() {
+
     }
 
     fun requestLocationPermission() {
@@ -70,69 +84,8 @@ class EditCaseActivity : AppCompatActivity() {
             )
             ActivityCompat.requestPermissions(this, strings, REQUEST_LOCATION_PERMISSION)
         } else {
-            requestLastLocation()
+            viewModel.requestLastLocation(this)
         }
-    }
-
-
-    fun requestLastLocation() {
-
-        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
-        fusedLocationProviderClient
-            .requestLocationUpdates(
-                getLocationRequest(),
-                object : LocationCallback() {
-                    override fun onLocationResult(locationResult: LocationResult) {
-                        locationResult.lastLocation?.let {
-                            Log.d("EditCaseActivity", "onLocationResult: $it")
-                            Log.d("EditCaseActivity", "onLocationResult: ${it.latitude}")
-                            Log.d("EditCaseActivity", "onLocationResult: ${it.longitude}")
-                        }
-                    }
-
-                },
-                Looper.getMainLooper()
-            ).addOnSuccessListener {
-                Log.d(TAG, "requestLastLocation: onSuccess")
-            }
-    }
-
-    fun checkDeviceLocation() {
-
-        val settingsClient = LocationServices.getSettingsClient(this)
-        val builder = LocationSettingsRequest.Builder()
-        val mLocationRequest = getLocationRequest()
-
-        builder.addLocationRequest(mLocationRequest)
-        val locationSettingsRequest = builder.build()
-        settingsClient.checkLocationSettings(locationSettingsRequest) // Define callback for success in checking the device location settings.
-            .addOnSuccessListener {
-                requestLocationPermission()
-            } // Define callback for failure in checking the device location settings.
-            .addOnFailureListener { e ->
-                // Device location settings do not meet the requirements.
-                val statusCode: Int = (e as ApiException).statusCode
-                when (statusCode) {
-                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> try {
-                        val rae: ResolvableApiException = e as ResolvableApiException
-                        // Call startResolutionForResult to display a pop-up asking the user to enable related permission.
-                        rae.startResolutionForResult(
-                            this@EditCaseActivity,
-                            REQUEST_LOCATION_PERMISSION
-                        )
-                    } catch (sie: SendIntentException) {
-                        // ...
-                    }
-                }
-            }
-    }
-
-    private fun getLocationRequest(): LocationRequest {
-        val mLocationRequest = LocationRequest()
-        mLocationRequest.interval = 5000
-        mLocationRequest.fastestInterval = 2000
-        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
-        return mLocationRequest
     }
 
 
@@ -145,7 +98,7 @@ class EditCaseActivity : AppCompatActivity() {
         when (requestCode) {
             REQUEST_LOCATION_PERMISSION -> {
                 if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                   requestLastLocation()
+                    viewModel.requestLastLocation(this)
                 } else {
                     Log.i(TAG, "Permission denied")
                 }
@@ -156,9 +109,12 @@ class EditCaseActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         when (requestCode) {
-
             REQUEST_SHOW_SETTINGS -> {
-                requestLocationPermission()
+                if (resultCode == Activity.RESULT_OK) {
+                    requestLocationPermission()
+                } else {
+                    Log.i(TAG, "User denied request")
+                }
             }
         }
     }
@@ -166,7 +122,7 @@ class EditCaseActivity : AppCompatActivity() {
     companion object {
         const val EXTRA_IS_NEW_CASE = "com.bluethunder.tar2.ui.edit_case.EXTRA_IS_NEW_CASE"
         private const val REQUEST_SHOW_SETTINGS = 123
-        private const val REQUEST_LOCATION_PERMISSION = 124
+        const val REQUEST_LOCATION_PERMISSION = 124
         private const val TAG = "EditCaseActivity"
 
         //https://developer.huawei.com/consumer/en/doc/development/HMSCore-Guides-V5/location-develop-steps-0000001050746143-V5#EN-US_TOPIC_0000001050746143__section090594025818
--- app/src/main/java/com/bluethunder/tar2/ui/edit_case/viewmodel/EditCaseViewModel.kt
+++ app/src/main/java/com/bluethunder/tar2/ui/edit_case/viewmodel/EditCaseViewModel.kt
@@ -1,11 +1,22 @@
 package com.bluethunder.tar2.ui.edit_case.viewmodel
 
+import android.app.Activity
+import android.content.IntentSender
+import android.location.Geocoder
+import android.location.Location
+import android.os.Looper
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.bluethunder.tar2.model.Resource
+import com.bluethunder.tar2.ui.edit_case.EditCaseActivity
+import com.huawei.hms.common.ApiException
+import com.huawei.hms.common.ResolvableApiException
+import com.huawei.hms.location.*
 import kotlinx.coroutines.launch
+import java.util.*
 
 
 class EditCaseViewModel : ViewModel() {
@@ -19,6 +30,16 @@ class EditCaseViewModel : ViewModel() {
 
     private val _dataLoading = MutableLiveData(false)
     val dataLoading: LiveData<Boolean> = _dataLoading
+
+
+    private val _deviceLocationCheck = MutableLiveData<Resource<String>>()
+    val deviceLocationCheck: LiveData<Resource<String>> = _deviceLocationCheck
+
+    private val _locationAddress = MutableLiveData<Resource<String>>()
+    val locationAddress: LiveData<Resource<String>> = _locationAddress
+
+    private val _lastLocation = MutableLiveData<Resource<Location>>()
+    val lastLocation: LiveData<Resource<Location>> = _lastLocation
 
 
     fun refresh() {
@@ -35,6 +56,88 @@ class EditCaseViewModel : ViewModel() {
             _onSelectedTabIndex.value = index
         }
 
+    }
+
+    fun checkDeviceLocation(activity: Activity) {
+        val settingsClient = LocationServices.getSettingsClient(activity)
+        val builder = LocationSettingsRequest.Builder()
+        val mLocationRequest = getLocationRequest()
+
+        builder.addLocationRequest(mLocationRequest)
+        val locationSettingsRequest = builder.build()
+        settingsClient.checkLocationSettings(locationSettingsRequest) // Define callback for success in checking the device location settings.
+            .addOnSuccessListener {
+                setDeviceLocationCheckValue(Resource.success("Location settings are satisfied."))
+            } // Define callback for failure in checking the device location settings.
+            .addOnFailureListener { e ->
+                // Device location settings do not meet the requirements.
+                when ((e as ApiException).statusCode) {
+                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> try {
+                        val rae: ResolvableApiException = e as ResolvableApiException
+                        // Call startResolutionForResult to display a pop-up asking the user to enable related permission.
+                        rae.startResolutionForResult(
+                            activity,
+                            EditCaseActivity.REQUEST_LOCATION_PERMISSION
+                        )
+                    } catch (sie: IntentSender.SendIntentException) {
+                        setDeviceLocationCheckValue(Resource.error("Unable to resolve location settings: ${sie.message}"))
+                    }
+                }
+            }
+    }
+
+    fun setDeviceLocationCheckValue(resource: Resource<String>) {
+        _deviceLocationCheck.value = resource
+    }
+
+
+    fun requestLastLocation(activity: Activity) {
+        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(activity)
+        fusedLocationProviderClient
+            .requestLocationUpdates(
+                getLocationRequest(),
+                object : LocationCallback() {
+                    override fun onLocationResult(locationResult: LocationResult) {
+                        locationResult.lastLocation?.let {
+                            Log.d("EditCaseActivity", "onLocationResult: $it")
+                            Log.d("EditCaseActivity", "onLocationResult: ${it.latitude}")
+                            Log.d("EditCaseActivity", "onLocationResult: ${it.longitude}")
+                            getLocationName(it, activity)
+                        }
+                    }
+                },
+                Looper.getMainLooper()
+            ).addOnSuccessListener {
+                Log.d(TAG, "requestLastLocation: onSuccess")
+            }
+    }
+
+    fun getLocationName(location: Location, activity: Activity) {
+        val geocoder = Geocoder(activity, Locale.getDefault())
+        val addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1)
+        if (addresses.isNotEmpty()) {
+            val address = addresses[0]
+            val addressText = StringBuilder()
+            for (i in 0 until address.maxAddressLineIndex) {
+                addressText.append(address.getAddressLine(i))
+                addressText.append("\n")
+            }
+            setAddressValue(Resource.success(addressText.toString()))
+        }
+
+    }
+
+    private fun setAddressValue(toString: Resource<String>) {
+        _locationAddress.value = toString
+    }
+
+
+    private fun getLocationRequest(): LocationRequest {
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.interval = 5000
+        mLocationRequest.fastestInterval = 2000
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        return mLocationRequest
     }
 
 }
