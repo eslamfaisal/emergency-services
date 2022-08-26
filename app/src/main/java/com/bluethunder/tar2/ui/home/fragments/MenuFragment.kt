142904cb9c09e1739b05f0a56df23e9aa72e6ebe
Eslam Faisal <eslam.faisal.ef@gmail.com>
2022-08-26 02:35:49 +0200

    fix temp things

--- app/src/main/java/com/bluethunder/tar2/ui/home/fragments/MenuFragment.kt
+++ app/src/main/java/com/bluethunder/tar2/ui/home/fragments/MenuFragment.kt
@@ -1,9 +1,10 @@
 package com.bluethunder.tar2.ui.home.fragments
 
+import android.app.Dialog
 import android.content.Intent
 import android.graphics.Paint
+import android.net.Uri
 import android.os.Bundle
-import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,16 +15,21 @@ import com.bluethunder.tar2.R
 import com.bluethunder.tar2.SessionConstants
 import com.bluethunder.tar2.databinding.FragmentMenuBinding
 import com.bluethunder.tar2.ui.auth.AuthActivity
+import com.bluethunder.tar2.ui.extentions.showLoadingDialog
 import com.bluethunder.tar2.utils.SharedHelper
 import com.bluethunder.tar2.utils.SharedHelperKeys
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.bumptech.glide.request.RequestOptions
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.huawei.agconnect.applinking.AppLinking
+import com.huawei.agconnect.applinking.ShortAppLinking
 import com.huawei.agconnect.auth.AGConnectAuth
 
 class MenuFragment : Fragment() {
 
     private lateinit var binding: FragmentMenuBinding
+    private lateinit var progressDialog: Dialog
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,6 +50,7 @@ class MenuFragment : Fragment() {
     }
 
     private fun initViews() {
+        progressDialog = requireActivity().showLoadingDialog()
         binding.usernameTv.text = SessionConstants.currentLoggedInUserModel!!.name
         val circularProgressDrawable = CircularProgressDrawable(requireActivity())
         circularProgressDrawable.strokeWidth = 5f
@@ -66,6 +73,56 @@ class MenuFragment : Fragment() {
             requireActivity().startActivity(Intent(requireActivity(), AuthActivity::class.java))
             requireActivity().finish()
         }
+
+        binding.shareApp.setOnClickListener {
+            shareAppLink("Download Tar2-طارق \n\nhttps://tar2.dra.agconnect.link/r-Tar2-App")
+        }
+        binding.privacyPolicy.setOnClickListener {
+            val url = "https://github.com/eslamfaisal/tar2-privacy-policy#readme"
+            val i = Intent(Intent.ACTION_VIEW)
+            i.data = Uri.parse(url)
+            startActivity(i)
+        }
+        binding.contactUs.setOnClickListener {
+            contactUS()
+        }
+        binding.aboutApp.setOnClickListener {
+            showAboutApp()
+        }
+    }
+
+    private fun showAboutApp() {
+        MaterialAlertDialogBuilder(requireActivity(),)
+            .setTitle(resources.getString(R.string.about_app))
+            .setMessage(resources.getString(R.string.about_app_description))
+            .setPositiveButton(resources.getString(R.string.ok)) { dialog, which ->
+                // Respond to positive button press
+                dialog.dismiss()
+            }
+            .show()
+    }
+
+    private fun contactUS() {
+
+        val content =
+            "User ID = ${SessionConstants.currentLoggedInUserModel!!.id}\n\n" + "Please type your message below\n\n"
+
+        val intent = Intent(Intent.ACTION_VIEW)
+        val data = Uri.parse("mailto:eslamfaisal423@gmail.com?subject=Tar2 App&body=$content")
+        intent.data = data
+        startActivity(intent)
+
+    }
+
+    private fun shareAppLink(shortUrl: String?) {
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, shortUrl)
+            type = "text/plain"
+        }
+
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
     }
 
     companion object {
