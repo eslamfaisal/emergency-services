80ca821a3a252f66e31ffc6efce17a31956e15cb
Eslam Faisal <eslam.faisal.ef@gmail.com>
2022-10-02 18:05:49 +0200

    update profile image

--- app/src/main/java/com/bluethunder/tar2/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/bluethunder/tar2/ui/profile/ProfileActivity.kt
@@ -1,20 +1,24 @@
 package com.bluethunder.tar2.ui.profile
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.telephony.TelephonyManager
+import android.view.View
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bluethunder.tar2.R
-import com.bluethunder.tar2.SessionConstants
 import com.bluethunder.tar2.databinding.ActivityProfileBinding
-import com.bluethunder.tar2.ui.auth.AuthActivity
 import com.bluethunder.tar2.ui.auth.model.UserModel
 import com.bluethunder.tar2.ui.extentions.getViewModelFactory
 import com.bluethunder.tar2.ui.profile.viewmodel.ProfileViewModel
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import com.github.dhaval2404.imagepicker.ImagePicker
 import me.ibrahimsn.lib.PhoneNumberKit
 
 class ProfileActivity : AppCompatActivity() {
@@ -36,11 +40,15 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     private fun initViewModel() {
-        viewModel.listenToUserDetails(SessionConstants.currentLoggedInUserModel!!.id!!)
+        viewModel.listenToUserDetails()
         viewModel.currentCaseUserDetails.observe(this) { userData ->
             userData?.let {
                 setUpUserData(it)
             }
+        }
+        viewModel.uploadingImage.observe(this) { reource ->
+            binding.profileProgress.visibility = if (reource) View.VISIBLE else View.GONE
+            binding.icEditProfile.visibility = if (reource) View.GONE else View.VISIBLE
         }
     }
 
@@ -66,6 +74,12 @@ class ProfileActivity : AppCompatActivity() {
 
     private fun initView() {
         setUpPhoneNumberTextField()
+        binding.editProfileView.setOnClickListener {
+            pickImage()
+        }
+        binding.backBtn.setOnClickListener {
+            finish()
+        }
     }
 
     private fun setUpPhoneNumberTextField() {
@@ -87,4 +101,43 @@ class ProfileActivity : AppCompatActivity() {
         )
     }
 
+    private val startForProfileImageResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            try {
+                val resultCode = result.resultCode
+                val data = result.data
+
+                when (resultCode) {
+                    Activity.RESULT_OK -> {
+                        //Image Uri will not be null for RESULT_OK
+                        val fileUri = data?.data!!
+                        viewModel.uploadProfileImage(fileUri.path!!)
+                    }
+                    ImagePicker.RESULT_ERROR -> {
+                        Toast.makeText(
+                            this, ImagePicker.getError(data), Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    else -> {
+
+                    }
+                }
+            } catch (e: Exception) {
+            }
+
+        }
+
+
+    private fun pickImage() {
+        ImagePicker.with(this).crop().compress(512).maxResultSize(
+                1080, 1080
+            ).createIntent { intent ->
+                startForProfileImageResult.launch(intent)
+            }
+    }
+
+    override fun onDestroy() {
+        viewModel.removeSnapshotListener()
+        super.onDestroy()
+    }
 }
--- app/src/main/java/com/bluethunder/tar2/ui/profile/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/bluethunder/tar2/ui/profile/viewmodel/ProfileViewModel.kt
@@ -4,9 +4,15 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.bluethunder.tar2.SessionConstants
+import com.bluethunder.tar2.cloud_db.CloudStorageWrapper
 import com.bluethunder.tar2.cloud_db.FirestoreReferences
+import com.bluethunder.tar2.model.Resource
 import com.bluethunder.tar2.ui.auth.model.UserModel
+import com.bluethunder.tar2.ui.edit_case.viewmodel.EditCaseViewModel
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ListenerRegistration
+import java.io.File
 
 
 class ProfileViewModel : ViewModel() {
@@ -16,23 +22,64 @@ class ProfileViewModel : ViewModel() {
     }
 
 
+    val userID = SessionConstants.currentLoggedInUserModel!!.id!!
+    var snapshotRegistration: ListenerRegistration? = null
+
     private val _currentCaseUserDetails = MutableLiveData<UserModel?>()
     val currentCaseUserDetails: LiveData<UserModel?> = _currentCaseUserDetails
 
-    fun listenToUserDetails(userID: String) {
+    private val _uploadingImage = MutableLiveData<Boolean>()
+    val uploadingImage: LiveData<Boolean> = _uploadingImage
 
-        FirebaseFirestore.getInstance()
-            .collection(FirestoreReferences.UsersCollection.value())
-            .document(userID)
-            .addSnapshotListener { value, error ->
-                if (error != null) {
-                    Log.e(TAG, "listenToComments: ", error)
-                } else {
-                    val case = value!!.toObject(UserModel::class.java)
-                    _currentCaseUserDetails.value = case
+
+    fun listenToUserDetails() {
+        snapshotRegistration =
+            FirebaseFirestore.getInstance().collection(FirestoreReferences.UsersCollection.value())
+                .document(userID).addSnapshotListener { value, error ->
+                    if (error != null) {
+                        Log.e(TAG, "listenToComments: ", error)
+                    } else {
+                        val case = value!!.toObject(UserModel::class.java)
+                        _currentCaseUserDetails.value = case
+                    }
                 }
+    }
+
+    fun uploadProfileImage(path: String) {
+        setMainImageLoading(true)
+
+        val reference =
+            CloudStorageWrapper.storageManagement.getStorageReference("profile_image/${System.currentTimeMillis()}.jpg")
+        val uploadTask = reference.putFile(File(path))
+        uploadTask.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                task.result.storage.downloadUrl.addOnSuccessListener {
+                    updateUserProfileImage(it.toString())
+                }.addOnFailureListener {
+                    setMainImageLoading(false)
+                }
+            } else {
+                setMainImageLoading(false)
+            }
+        }
+    }
+
+    fun updateUserProfileImage(url: String){
+        FirebaseFirestore.getInstance().collection(FirestoreReferences.UsersCollection.value())
+            .document(userID).update(
+                mapOf("imageUrl" to url)
+            ).addOnCompleteListener {
+                Log.d(TAG, "updateUserProfileImage: isSuccessful = ${it.isSuccessful}")
+                setMainImageLoading(false)
             }
     }
 
+    private fun setMainImageLoading(loading: Boolean) {
+        _uploadingImage.value = loading
+    }
 
+
+    fun removeSnapshotListener() {
+        snapshotRegistration?.remove()
+    }
 }
